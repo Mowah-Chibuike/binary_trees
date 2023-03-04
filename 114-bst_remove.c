6f760e0352a65546d5bb4bb75e14279304d6c272
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-04 14:44:20 +0000

    added the 114-bst_remove.c file

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -50,7 +50,7 @@ bst_t *bst_remove(bst_t *root, int value)
 		else
 		{
 			root->n = findMin(root->right);
-			bst_remove(root->right, root->n);
+			root->right = bst_remove(root->right, root->n);
 		}
 	}
 	return (root);
